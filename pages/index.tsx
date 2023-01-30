23413ee9041f926f940fc03583162c07864dee80
Junho Park <dwarfthema@gmail.com>
2023-01-31 08:33:51 +0900

    update:main:3step motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,17 +15,28 @@ import UseFollowPointer from "../components/useFollowPointer";
 const inter = Inter({ subsets: ["latin"] });
 
 const mainBoxVariants: Variants = {
+  up: {
+    opacity: 1,
+    scaleY: 0,
+    y: "-100%",
+    transition: { type: "tween", duration: 1, ease: "easeInOut" },
+  },
+
   open: {
     opacity: 1,
     scaleY: 1,
     y: 0,
-    transition: { stiffness: 1000, velocity: -100 },
+    transition: {
+      type: "tween",
+      duration: 0.5,
+      ease: "easeInOut",
+    },
   },
-  closed: {
-    opacity: 0,
+  down: {
+    opacity: 1,
     scaleY: 0,
     y: "100%",
-    transition: { stiffness: 1000, velocity: -100 },
+    transition: { type: "tween", duration: 1, ease: "easeInOut" },
   },
 };
 
@@ -33,49 +44,91 @@ export default function Home() {
   const mouseFollower = useRef(null);
   const { x, y } = UseFollowPointer(mouseFollower);
 
+  const [enablePage, setEnablePage] = useState<boolean>(true);
   const [page, setPage] = useState<number>(0);
+  const pageRef = useRef(0);
+  const enableRef = useRef(true);
   const TOTAL_PAGE = 3;
 
   useEffect(() => {
-    window.addEventListener("wheel", changePage);
+    window.addEventListener("wheel", (e) => changePage(e));
 
     function changePage(e: WheelEvent) {
-      if (page !== TOTAL_PAGE) {
-        setPage((page + 1) % TOTAL_PAGE);
-      } else {
-        setPage(0);
+      //console.log(e);
+
+      if (enableRef.current) {
+        console.log(enableRef.current, ": true");
+
+        if (
+          e.deltaY >= 0 &&
+          e.deltaY <= 30 &&
+          pageRef.current < TOTAL_PAGE - 1
+        ) {
+          pageRef.current += 1;
+
+          setTimeout(() => {
+            setEnablePage(true);
+            enableRef.current = true;
+          }, 1000);
+
+          enableRef.current = false;
+          setEnablePage(false);
+        }
+        if (e.deltaY <= 0 && pageRef.current > 0) {
+          setTimeout(() => {
+            setEnablePage(true);
+            enableRef.current = true;
+          }, 1000);
+
+          pageRef.current -= 1;
+
+          setEnablePage(false);
+          enableRef.current = false;
+        }
       }
     }
-  }, [page]);
+  }, [pageRef.current]);
 
   return (
     <Layout seoTitle="VivlePark">
-      <div className="h-screen bg-black text-white">
+      <motion.div
+        ref={mouseFollower}
+        className=" h-6 w-6 absolute rounded-full xl:border-red-300 border-dotted xl:border-2 lg:border-0 z-40 "
+        animate={{ x, y }}
+        transition={{
+          type: "spring",
+          damping: 50,
+          stiffness: 400,
+        }}
+      />
+      <div className="h-screen text-white">
         <motion.div
           className="h-full w-full bg-green-400 absolute top-0"
           variants={mainBoxVariants}
-          animate={page === 2 ? "open" : "closed"}
-        ></motion.div>
+          animate={
+            pageRef.current === 2 ? "open" : pageRef.current < 2 ? "down" : "up"
+          }
+        >
+          2
+        </motion.div>
         <motion.div
           className="h-full w-full bg-blue-400 absolute top-0"
           variants={mainBoxVariants}
-          animate={page === 1 ? "open" : "closed"}
-        ></motion.div>
+          animate={
+            pageRef.current === 1 ? "open" : pageRef.current < 1 ? "donw" : "up"
+          }
+        >
+          1
+        </motion.div>
         <motion.div
           className="h-screen w-screen bg-red-400 absolute top-0"
           variants={mainBoxVariants}
-          animate={page === 0 ? "open" : "closed"}
-        ></motion.div>
-        <motion.div
-          ref={mouseFollower}
-          className=" h-6 w-6 absolute rounded-full border-red-300 border-dotted border-2"
-          animate={{ x, y }}
-          transition={{
-            type: "spring",
-            damping: 50,
-            stiffness: 400,
-          }}
-        />
+          animate={
+            pageRef.current === 0 ? "open" : pageRef.current < 0 ? "null" : "up"
+          }
+        >
+          0
+        </motion.div>
       </div>
     </Layout>
   );
