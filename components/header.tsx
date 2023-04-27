c8248a730d2c0795076215b276e28cfe1e54f114
Junho Park <dwarfthema@gmail.com>
2023-04-27 13:31:10 +0900

    fix header logo img

--- components/header.tsx
+++ components/header.tsx
@@ -4,19 +4,7 @@ import Image from "next/image";
 import Logo from "../public/images/logo_double_wt.png";
 import WhiteLogo from "../public/images/logo_single.png";
 
-const scrollDownEvent = (height: number) => {
-  console.log(height);
-  if (typeof window !== "undefined") {
-    window.scrollTo({
-      top: height,
-      behavior: "smooth",
-    });
-  }
-};
-
 const Header = () => {
-  const [isScroll, setScroll] = useState(false);
-
   return (
     <>
       <header
@@ -26,10 +14,10 @@ const Header = () => {
           <Image
             src={WhiteLogo}
             alt="VivlePark"
-            className="max-h-10 max-w-fit sm:max-h-6 sm: no-drag "
+            className="w-fit max-h-10 sm:max-h-6 no-drag "
           />
         </div>
-        <div className="flex justify-end z-20 w-full  text-xl  ">
+        <div className="flex justify-end z-20 w-full text-xl">
           <Link legacyBehavior href="https://open.kakao.com/me/Vivlepark">
             <a id="link" target="_blank" className="">
               <div className="text-white font-bold sm:text-base bg-black bg-opacity-25 px-2 py-[2px] rounded-lg">
